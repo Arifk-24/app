a1e2b0273fb920f1a661a533b21fb9038e71be48
Arif khan <69386772+Arifk-24@users.noreply.github.com>
2020-09-28 17:07:32 +0530

    Updates

---
+++ precache-manifest.a8327da0510dcbe631d887375d4136de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06eb6d58c13fc25ff8970e42a3216097",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "cffaebe290d64c872a0c",
+    "url": "/app/static/css/main.a349f36e.chunk.css"
+  },
+  {
+    "revision": "1bdb40bf5da882de204c",
+    "url": "/app/static/js/2.e4188188.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.e4188188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cffaebe290d64c872a0c",
+    "url": "/app/static/js/main.167ffcb8.chunk.js"
+  },
+  {
+    "revision": "fb683d993c694aeb9312",
+    "url": "/app/static/js/runtime-main.95af9303.js"
+  }
+]);
