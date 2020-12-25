e86db7ca49ab1e694522996493599c7f73f83e67
Arif khan <arifk240901@gmail.com>
2020-12-25 23:50:54 +0530

    n

--- src/App.js
+++ src/App.js
@@ -11,79 +11,16 @@ class Square extends React.Component{
 	}
 }
 class Board extends React.Component{
-	constructor(props){
-		super(props); 
-		this.state={
-			square : Array(9).fill(null),
-			Isxturn: true
-		};
-	}
-	declarewinner(square){
-		const list=[
-		[0,1,2],
-		[3,4,5],
-		[6,7,8],
-		[0,3,6],
-		[1,4,7],
-		[2,5,8],
-		[0,4,8],
-		[2,4,6]
-		];
-		for(let i=0;i<list.length;i++)
-		{
-		const [a,b,c]=list[i];
-		if(square[a]===square[b] && square[b]===square[c] && square[c]===square[a])
-			return square[b];
-		}
-		return null;
-	}
-	handleClick(i){
-		const square = this.state.square.slice();
-		if(this.declarewinner(square) || square[i])
-			return;
-		square[i]=this.state.Isxturn?"X": "O";
-		this.setState({
-			square: square,
-			Isxturn: !this.state.Isxturn
-		});
-	}
-	/*
-	declarewinner(square){
-		const list=[
-		[0,1,2],
-		[3,4,5],
-		[6,7,8],
-		[0,3,6],
-		[1,4,7],
-		[2,5,8],
-		[0,4,8],
-		[2,4,6]
-		];
-		for(let i=0;i<list.length;i++)
-		{
-		const [a,b,c]=list[i];
-		if(square[a]===square[b] && square[b]===square[c] && square[c]===square[a])
-			return square[b];
-		}
-		return null;
-	}
-	*/
+
 	rendersquare(i){
 		return <Square 
-		value= {this.state.square[i]}	
-		onClick={()=>this.handleClick(i)}
+		value= {this.props.square[i]}	
+		onClick={()=>this.props.onClick(i)}
 		/>;
 	}
 	render(){
-		let status;
-		let winner= this.declarewinner(this.state.square);
-		if(winner)
-			status= "Winner is :"+ winner;
-		else
-			status= "player status: "+ (this.state.Isxturn?"X":"O");
 		return(
 		<div>
-		<p class = " status">{status}</p>
 		<table class = "board">		
 		<tr>	    
 		<td>{this.rendersquare(0)}</td>
@@ -106,12 +43,69 @@ class Board extends React.Component{
 	}
 }
 class App extends React.Component{
+	constructor(props){
+		super(props); 
+		this.state={
+			history:[
+				{square: Array(9).fill(null),}
+			],
+			Isxturn: true
+		};
+	}
+
+	handleClick(i){
+		const history= this.state.history;
+		const current= history[history.length-1];
+		const square = current.square.slice();
+		if(this.declarewinner(square) || square[i])
+			return;
+		square[i]=this.state.Isxturn?"X": "O";
+		this.setState({
+			history: history.concat([{
+				square:square,
+			}]),
+			Isxturn: !this.state.Isxturn
+		});
+	}
+
+	declarewinner(square){
+		const list=[
+		[0,1,2],
+		[3,4,5],
+		[6,7,8],
+		[0,3,6],
+		[1,4,7],
+		[2,5,8],
+		[0,4,8],
+		[2,4,6]
+		];
+		for(let i=0;i<list.length;i++)
+		{
+		const [a,b,c]=list[i];
+		if(square[a]&& square[a]===square[b] && square[a]===square[c])
+			return square[a];
+		}
+		return null;
+	}
 	render(){
+		const history= this.state.history;
+		const current= history[history.length-1];
+		const square= current.square.slice();
+		let status;
+		const winner= this.declarewinner(square);
+		if(winner)
+			status= "Winner is :"+ winner;
+		else
+			status= "player status: "+ (this.state.Isxturn?"X":"O");
 		return(
 		<div>
 		<h1>Welcome to the Game </h1>
 		<div >
-		<Board/>
+		<p class ="status">{status}</p>
+		<Board
+		square ={square}
+		onClick={i => this.handleClick(i)}
+		/>
 		</div>
 		</div>
 		);
